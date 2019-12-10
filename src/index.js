b1092704d0d7d87754f45ae71d4c65f1ce7bd827
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2019-12-09 17:04:12 -0800

    setting up work space

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./css/index.css";
+import "./css/reset.css";
+import App from "./component/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
