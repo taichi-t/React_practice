962ea6b8570458d6d9a335f7de69e23e071032bb
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2019-12-21 21:57:55 -0800

    some change

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -14,7 +14,7 @@ class About extends Component {
             <h2 className="about_title">About</h2>
           </Grid>
 
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <Reveal animation={Animation.FadeInUp} delay={250}>
               <div className="container">
                 <img src={me} alt="me" className="img" />
@@ -26,23 +26,51 @@ class About extends Component {
             <Reveal animation={Animation.FadeInUp} delay={250}>
               <div className="container">
                 <div className="me_discription">
-                  My name is “Taichi Tsujioka”. I’m a Front-end Developer,
-                  currently living in Vancouver, Canada. I’m really passionate
-                  about building and designing websites with up-to-date
-                  programming languages and technologies. On my daily basis, I
-                  constantly learn new programming languages and brush up on my
-                  skills. When I’m facing bugs and problems, I manage to find
-                  the most efficient and the best way to solve the issues with a
-                  logical mindset
-                </div>
-
-                <div className="resume_button_container">
-                  <button type="button" className="resume_button">
-                    <span className="button_text">Resume</span>
-                  </button>
+                  <h5>I'm really passionate about</h5>
+                  <p>
+                    codingand designing. I've always enjoyed being creative,
+                    ever since I was a child. I get a sense of satisfaction and
+                    joy whenever what I create brings others happiness.
+                  </p>
+                  <h5>My ambition is </h5>
+                  <p>
+                    to create my own web service, so I'm very interested in
+                    back-end programming. I believe in continuous learning and
+                    am always looking to brush up my skills. I see myself
+                    becoming a web developer who understands the ins and outs of
+                    what back-end developers and designers do as well.
+                  </p>
                 </div>
               </div>
             </Reveal>
+          </Grid>
+          <Grid item xs={6}>
+            <Reveal animation={Animation.FadeInUp} delay={250}>
+              <div className="container">
+                <div className="me_discription">
+                  <h5>When I work on a project,</h5>
+                  <p>
+                    I'm a great team player. I'm the sort of person who helps
+                    others when they're stuck with buggy codes and manages to
+                    find efficient solutions logically. On top of that, I'm
+                    always on time.
+                  </p>
+                  <h5>Me time involves</h5>
+                  <p>
+                    taking photos—especially of interesting architecture—working
+                    out, coming up with new beats, and camping. I also love
+                    coding and programming in my spare time.
+                  </p>
+                </div>
+              </div>
+            </Reveal>
+          </Grid>
+          <Grid>
+            <div className="resume_button_container">
+              <button type="button" className="resume_button">
+                <span className="button_text">Resume</span>
+              </button>
+            </div>
           </Grid>
         </Grid>
       </div>
